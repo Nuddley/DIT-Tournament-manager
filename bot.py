350dd03699d409fc2c5c74c04859b39e83782a73
Noah <19ngreen@wakatipu.school.nz>
2023-06-29 15:05:28 +1200

    Version 1.0

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
+"""Version 1.0 Tourni Bot."""
+
 import discord
 from discord.ext import commands
+from tkinter import *
+
 
 TOKEN = "Enter Token"
-
-
 client = commands.Bot(command_prefix="!", intents = discord.Intents.all())
-
 entrys = []
 
 @client.event
@@ -22,34 +23,38 @@ async def me(ctx):
     message = message_content[4:]
     user = ctx.author
 
-    temp_list = []
-    for letter in message:
-        temp_list.append(letter)
-        name_rank.append(letter)
-    temp_list.remove("(")
-    temp_list.remove("(")
-    name_rank.remove("(")
-    name_rank.remove("(")
+    try:
+        temp_list = []
+        for letter in message:
+            temp_list.append(letter)
+            name_rank.append(letter)
+        temp_list.remove("(")
+        temp_list.remove("(")
+        name_rank.remove("(")
+        name_rank.remove("(")
 
-    for x in temp_list:
-        if x == ")":
-            name_rank.remove(x)
-            break
-        else:
-            ingame_name += x
-            name_rank.remove(x)
+        for x in temp_list:
+            if x == ")":
+                name_rank.remove(x)
+                break
+            else:
+                ingame_name += x
+                name_rank.remove(x)
 
-    for x in name_rank:
-        if x == ")":
-            break
-        else:
-            ingame_rank += x
+        for x in name_rank:
+            if x == ")":
+                break
+            else:
+                ingame_rank += x
 
-    print(f"Both: {name_rank}")
-    print(f"Name: {ingame_name}")
-    print(f"Rank: {ingame_rank}")
+        print(f"Both: {name_rank}")
+        print(f"Name: {ingame_name}")
+        print(f"Rank: {ingame_rank}")
+        print(f"Discord Name: {user}")
 
-    await ctx.author.send(f"Your have entered the tournament with the following information: \nUsername:{ingame_name} \nRank: {ingame_rank}")
+        await ctx.author.send(f"Hello {user} you have entered the tournament with the following information: \nUsername:{ingame_name} \nRank: {ingame_rank}")
+    except:
+        await ctx.send(f"{ctx.message.author.mention} That format is incorrect, please try again with the following format. \n!me (UserName)(Rank)")
 
     entrys.append([ctx.author, ingame_name, ingame_rank])
 
